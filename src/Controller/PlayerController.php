103ae5434dc9d718c046724693779aa846469c01
Mariam <codegirlbxl@gmail.com>
2026-05-17 14:29:43 +0200

    make var_dupm to see the data from database

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\PlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+// use Symfony\Component\HttpFoundation\Request; l'ho utilizzata per il debug ma non fonziona
+
 final class PlayerController extends AbstractController
 {
-    #[Route('/player', name: 'app_player')]
-    public function index(): Response
+    // #[Route('/player', name: 'app_player')]
+    // public function index(): Response
+    // {
+    //     return $this->render('player/index.html.twig', [
+    //         'controller_name' => 'PlayerController',
+    //     ]);
+    // }
+
+
+    #[Route('/players', name: 'app_player')]
+    public function index(PlayerRepository $repository): Response
     {
-        return $this->render('player/index.html.twig', [
-            'controller_name' => 'PlayerController',
-        ]);
+        $players = $repository->findAll();
+        dd($players);
+
+        // dump($request);  non mi trova la variabile riquest
+        // die;
     }
 }

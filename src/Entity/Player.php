3b8c674f8dce0301d415445a8f677d37555c3ff3
Mariam <codegirlbxl@gmail.com>
2026-05-17 15:34:39 +0200

    add page show.html.twig

--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -29,6 +29,9 @@ class Player
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $image = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -90,6 +93,18 @@ class Player
     public function setImage(?string $image): static
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
 
         return $this;
     }

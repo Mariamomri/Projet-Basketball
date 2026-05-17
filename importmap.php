051733b1a7a6c0795b531fe83f6846c9249ee84e
Mariam <mariamomri.dev@gmail.com>
2026-05-17 16:21:17 +0200

    install tailwind-css

--- importmap.php
+++ importmap.php
@@ -25,4 +25,11 @@ return [
     '@hotwired/turbo' => [
         'version' => '8.0.23',
     ],
+    'tailwindcss' => [
+        'version' => '4.3.0',
+    ],
+    'tailwindcss/index.min.css' => [
+        'version' => '4.3.0',
+        'type' => 'css',
+    ],
 ];

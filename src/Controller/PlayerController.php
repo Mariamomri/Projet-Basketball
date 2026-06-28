807aec5eb7dbbfbd52ba32bc256e314678348f69
Mariam <mariamomri.dev@gmail.com>
2026-06-28 21:11:52 +0200

    feat: add create, edit, delete actions to PlayerController

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -9,6 +9,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 use Symfony\Component\HttpFoundation\JsonResponse; //return json 
 
+use App\Entity\Player;
+use App\Form\PlayerType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 // use Symfony\Component\HttpFoundation\Request; l'ho utilizzata per il debug ma non fonziona
 
 final class PlayerController extends AbstractController
@@ -57,4 +62,57 @@ final class PlayerController extends AbstractController
             'slug' => $slug,
         ]);
     } // in url faire http://127.0.0.1:8000/playersjson/lebron-james-1
+
+    #[Route('/players/create', name: 'app_player_create')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $player = new Player();
+        $form = $this->createForm(PlayerType::class, $player);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $player->setCoach($this->getUser());
+            $em->persist($player);
+            $em->flush();
+
+            $this->addFlash('success', 'Le joueur ' . $player->getName() . ' a bien été créé');
+
+            return $this->redirectToRoute('app_player');
+        }
+
+        return $this->render('player/create.html.twig', [
+            'monForm' => $form,
+        ]);
+    }
+
+    #[Route('/players/{id}/edit', name: 'app_player_edit', requirements: ['id' => '\d+'])]
+    public function edit(Player $player, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(PlayerType::class, $player);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Le joueur ' . $player->getName() . ' a bien été modifié');
+
+            return $this->redirectToRoute('app_player');
+        }
+
+        return $this->render('player/edit.html.twig', [
+            'monForm' => $form,
+            'player' => $player,
+        ]);
+    }
+
+    #[Route('/players/{id}/delete', name: 'app_player_delete', requirements: ['id' => '\d+'])]
+    public function delete(Player $player, EntityManagerInterface $em): Response
+    {
+        $em->remove($player);
+        $em->flush();
+
+        $this->addFlash('success', 'Le joueur a bien été supprimé');
+
+        return $this->redirectToRoute('app_player');
+    }
 }

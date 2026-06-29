d4250fb217f2a3c1107a0d17a824f57c29e349ab
Mariam <mariamomri.dev@gmail.com>
2026-06-29 13:45:02 +0200

    feat: add validation constraints to Coach entity and registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,16 +11,24 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
+            ->add('firstname', TextType::class, [
+                'label' => 'registerForm.firstname',
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'registerForm.lastname',
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'registerForm.email',
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'registerForm.agreeTerms',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue(
@@ -29,8 +37,7 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+                'label' => 'registerForm.plainPassword',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -40,12 +47,10 @@ class RegistrationFormType extends AbstractType
                     new Length(
                         min: 6,
                         minMessage: 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
                         max: 4096,
                     ),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void

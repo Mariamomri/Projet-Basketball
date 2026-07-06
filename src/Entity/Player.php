c2e75f31abc6cfa10940a42d9492ba55ed1e1b7c
Mariam <mariamomri.dev@gmail.com>
2026-07-06 18:55:51 +0200

    Add image upload fields to Player entity

--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -6,10 +6,13 @@ use App\Repository\PlayerRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
 
 #[ORM\Entity(repositoryClass: PlayerRepository::class)]
 #[ORM\Table(name: 'players')]
 #[ORM\HasLifecycleCallbacks]
+#[Vich\Uploadable]
 class Player
 {
     #[ORM\Id]
@@ -50,6 +53,15 @@ class Player
     #[ORM\ManyToOne(inversedBy: 'players')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Coach $coach = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
+
+    #[Vich\UploadableField(mapping: 'player_images', fileNameProperty: 'imageName', size: 'imageSize')]
+    private ?File $imageFile = null;
 
     public function getId(): ?int
     {
@@ -173,5 +185,38 @@ class Player
         $this->coach = $coach;
 
         return $this;
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+        if ($imageFile) {
+            $this->setUpdatedAt(new \DateTimeImmutable());
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageSize(?int $imageSize): void
+    {
+        $this->imageSize = $imageSize;
+    }
+
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
     }
 }
